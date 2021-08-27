dda043e46b600f49438fb4c9a44eaf9ea9e255ea
denistss <denists88@gmail.com>
2021-08-26 22:47:35 -0300

    Criado tela inicial de login; Instalado bootstrap e font-awesome;

---
+++ src/app/login-input/login-input.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-input',
+  templateUrl: './login-input.component.html',
+  styleUrls: ['./login-input.component.css']
+})
+export class LoginInputComponent {
+  title = '';
+}
