5a2edf6145dc5bbbf2a2e1946fc8a9cc3ace3eec
ImpactaDenis <denis.tiago@aluno.faculdadeimpacta.com.br>
2021-09-05 22:57:43 -0300

    Limpeza de componentes nao utilizado e organização de pastas;

--- delivery-website/src/app/app.module.ts
+++ delivery-website/src/app/app.module.ts
@@ -9,9 +9,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Error404Component } from './error-404/error-404.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { ModalChangeStatusComponent } from './nav-bar/modal/modal-change-status.component';
-import { SidenavComponent } from './navigation/sidenav.component';
+import { ModalChangeStatusComponent } from './navigation/modal-change-status/modal-change-status.component';
 import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
 import { MaterialModule } from './material.module';
 import { HeaderComponent } from './navigation/header/header.component';
@@ -21,9 +19,7 @@ import { HeaderComponent } from './navigation/header/header.component';
   declarations: [
     AppComponent,
     Error404Component,
-    NavBarComponent,
     ModalChangeStatusComponent,
-    SidenavComponent,
     HeaderComponent,
     SidenavListComponent
   ],
